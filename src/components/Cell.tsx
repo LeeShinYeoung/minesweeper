5e645113428db253859cd7789c60ec020a10d129
LeeShinYeoung <iqrash12@gmail.com>
2022-09-18 21:49:04 +0900

    Develop a cell container

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,11 +1,62 @@
-function CellContainer({ x, y, grid, openCell, flagCell }: any) {
-  const key = `${x}:${y}`
+import React from 'react'
+import { Cell, CellContainerProps, Grid } from '../interfaces'
+import { CoordinateString } from '../types'
+
+function CellContainer({
+  x,
+  y,
+  grid,
+  listenOpenCell,
+  listenFlagCell,
+}: CellContainerProps) {
+  const key: CoordinateString = `${x}:${y}`
 
   return (
-    <div className="minesweeper-cell" key={key}>
-      {x} {y}
+    <div
+      key={key}
+      className={`minesweeper-cell ${getCellClass(grid[key])}`}
+      onClick={clickCell}
+      onContextMenu={clickCell}
+    >
+      {getCellContent(grid[key])}
     </div>
   )
+
+  function getCellClass({ is_opened }: Pick<Cell, 'is_opened'>) {
+    const classNames = ['cell']
+
+    if (is_opened) {
+      classNames.push('opened')
+    }
+
+    return classNames.join(' ')
+  }
+
+  function getCellContent({
+    is_flagged,
+    is_opened,
+    is_bomb,
+    close_bombs,
+  }: Pick<Cell, 'is_flagged' | 'is_opened' | 'is_bomb' | 'close_bombs'>) {
+    if (is_flagged) {
+      return '🚩'
+    } else if (!is_opened) {
+      return ''
+    } else if (is_bomb) {
+      return '💣'
+    } else if (close_bombs) {
+      return close_bombs + ''
+    }
+  }
+
+  function clickCell(event: React.MouseEvent<HTMLElement>) {
+    event.preventDefault()
+    if (event.type === 'click') {
+      listenOpenCell(x, y)
+    } else if (event.type === 'contextmenu') {
+      listenFlagCell(x, y)
+    }
+  }
 }
 
 export default CellContainer
